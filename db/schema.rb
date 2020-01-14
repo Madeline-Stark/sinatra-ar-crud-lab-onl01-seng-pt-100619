96756bd0afca27fd4c8037923c90f3ac362767ad
Madeline Stark <madeline.stark@flatironschool.com>
2020-01-14 23:42:26 +0000

    Done.

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2015_12_01_201114) do
+ActiveRecord::Schema.define(version: 2020_01_14_233713) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.string "content"
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "name"
+    t.integer "grade"
   end
 
 end
