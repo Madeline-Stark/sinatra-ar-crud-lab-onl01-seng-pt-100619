361102180cf94356bc14f8b1376149f903eb2e95
Madeline Stark <madeline.stark@flatironschool.com>
2020-01-15 02:45:26 +0000

    Automatically backed up by Learn

--- db/migrate/20151201201114_create_articles.rb
+++ db/migrate/20151201201114_create_articles.rb
@@ -3,7 +3,7 @@ class CreateArticles < ActiveRecord::Migration[5.1]
     create_table :articles do |t|
       t.string :title
       t.string :content
-
+      t.integer :teacher_id
     end
   end
 end
